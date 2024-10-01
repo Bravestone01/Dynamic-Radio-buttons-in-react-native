b257b89f98c1a1d28488eed66e461db76a2a4952
Bravestone01 <mujtabashah906@gmail.com>
2024-10-01 17:41:29 +0500

    initial commi

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const skills=[
+    {
+      id:1,
+      name:"Java"
+    },
+    {
+      id:2,
+      name:"PhP  ",
+    },
+    {
+      id:3,
+      name:"C#    "
+    },
+    {
+      id:4,
+      name:"C++ "
+    }
+  ]
+  const [radio, setRadio] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={styles.main}>
+        {
+          skills.map((item,index)=>
+          <TouchableOpacity key={index} onPress={() => setRadio(item.id)} >
+          <View style={styles.radioWraper}>
+            <View style={styles.radio}>
+              {radio == item.id ? <View style={styles.radiobg}></View> : null}
+            </View>
+            <Text style={styles.radioText}>{item.name}</Text>
+          </View>
+        </TouchableOpacity>)
+        }
+
+        
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  radioText: {
+    fontSize: 20,
+  },
+  radio: {
+    width: 30,
+    height: 30,
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 15,
+    margin: 10,
+  },
+  radioWraper: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  radiobg: {
+    backgroundColor: "blue",
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    margin: 4.3,
   },
 });
